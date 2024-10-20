662f34192624f09db03984f180431e2ea9b7e907
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-20 10:34:43 +0530

    made dir for static to keep our assets such as css files images and etcc

--- OpEngine/settings.py
+++ OpEngine/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -125,3 +125,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/' 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+
+#to keep assets in static folder# 
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
