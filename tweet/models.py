81069edc57757aa70c54d559c2d8acd8d9d679db
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-21 12:19:42 +0530

    gonna create the model now , and import the User from admin panel

--- tweet/models.py
+++ tweet/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
+#importing the User from admin panel , this will used to create model and store the data#
+from django.contrib.auth.models import User;
+
 # Create your models here.
