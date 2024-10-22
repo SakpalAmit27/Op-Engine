115fdfade08f80443957093bdb815658aadc49d3
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-22 07:57:25 +0530

    made our form

--- tweet/forms.py
+++ tweet/forms.py
@@ -1,1 +1,11 @@
 #created forms.py 
+
+from django import forms
+
+from .models import Tweet;
+
+class TweetForm(forms.ModelForm):
+    class Meta:
+        model = Tweet,
+        fields = ['text','photo'],
+        
