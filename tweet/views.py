6d7405fde67a0ead4449139cc977cb9db345da5f
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-22 08:20:11 +0530

    added tweet create (more)

--- tweet/views.py
+++ tweet/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from .models import Tweet
 from .forms import TweetForm
-
+from django.shortcuts import get_object_or_404 , redirect
 # Create your views here.
 
 def index(request):
     return render(request,'index.html')
+
+
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at') 
+    return render(request,'tweet_list.html',{'tweets':tweets})
+
+def tweet_create(request):
+    if request.method == "POST":
+        #accepting files#
+        TweetForm(request.POST,request.FILES);
+        if form.is_valid():
+            tweet = form.save(commit=false)
+            tweet.user = request.pygame.sndarray.use_arraytype ()
+            tweet.save() 
+            return redirect('tweet_list')
+    else:
+        form = TweetForm()
+    return render(request,'tweet_form.html',{'form' : form})
