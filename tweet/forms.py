9cc150916b40eaeb459d853be84cfdd49c94bb9a
Sakpal Amit <amitsakpal744@gmail.com>
2024-10-29 19:46:59 +0530

    custom input styles changes

--- tweet/forms.py
+++ tweet/forms.py
@@ -7,5 +7,8 @@ from .models import Tweet;
 class TweetForm(forms.ModelForm):
     class Meta:
         model = Tweet
-        fields = ['text','photo']
+        fields = ['text', 'photo']  # Include other fields as needed
         
+        widgets = {
+            'text': forms.TextInput(attrs={'class': 'input input-bordered w-full'}),
+            'photo': forms.ClearableFileInput(attrs={'class': 'file-input file-input-bordered w-full'}),}
